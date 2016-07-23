80f85e50ec66b6c215fe5f7ea5b8be134a780c8b
medright <medright@gmail.com>
2016-07-23 11:10:22 -0400

    add Google OAuth2 signup/signin

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+def index
+    if params[:approved] == "false"
+      @users = User.find_by_approved(false)
+    else
+      @users = User.all
+    end
+  end
--- app/models/user.rb
+++ app/models/user.rb
@@ -29,14 +29,23 @@ class User < ApplicationRecord
   end
 
   def self.from_omniauth(auth)
-
-    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
-      user.email
+      user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.save!
     end
+  end
 
+  def self.new_with_session(params, session)
+    if session["devise.user_attributes"]
+      new(session["devise.user_attributes"], without_protection: true) do |user|
+        user.attributes = params
+        user.valid?
+      end
+    else
+      super
+    end
   end
 end
