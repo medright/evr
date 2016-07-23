b876c19ee4525a654afce1e54ba61007ada14f9b
medright <medright@gmail.com>
2016-07-23 11:41:46 -0400

    add correct columns to users table for lockable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160718150759) do
+ActiveRecord::Schema.define(version: 20160723153111) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -28,6 +28,9 @@ ActiveRecord::Schema.define(version: 20160718150759) do
     t.boolean  "approved"
     t.string   "provider"
     t.string   "uid"
+    t.integer  "failed_attempts",        default: 0
+    t.string   "unlock_token"
+    t.datetime "locked_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
